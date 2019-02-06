da8300bf2b3e3bb23bd38407e3a653050a463386
Adam Rush <adamrushuk@gmail.com>
2019-02-06 15:58:08 +0000

    Fixed default admin user

--- clone-windows-vm/variables.tf
+++ clone-windows-vm/variables.tf
@@ -14,7 +14,7 @@ variable "vm" {
 
   default = {
     template_name  = "w2012r2-base"
-    admin_user     = "administrator@vsphere.local"
+    admin_user     = "Administrator"
     admin_password = "Pa55word!"
     count          = "1"
   }
